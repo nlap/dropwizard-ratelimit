f157747764a2b7d1dbf6e136ee019e93df094b6d
Nathan Lapierre <nathan@lapierre.ca>
2014-12-02 20:05:20 -0400

    initial working rate limiting

--- bundle/src/main/java/ca/nlap/dropwizard/ratelimit/RateLimitBundle.java
+++ bundle/src/main/java/ca/nlap/dropwizard/ratelimit/RateLimitBundle.java
@@ -1,11 +1,17 @@
 package ca.nlap.dropwizard.ratelimit;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 import io.dropwizard.ConfiguredBundle;
+import io.dropwizard.lifecycle.Managed;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.Configuration;
 import io.dropwizard.setup.Environment;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
+
+import java.net.URL;
 
 /**
  * This dropwizard bundle adds support for rate limiting.
@@ -17,12 +23,33 @@ public abstract class RateLimitBundle<T extends Configuration> implements Config
 
 	@Override
 	public void run(T configuration, Environment environment) throws Exception {
+
+		environment.lifecycle().manage(new Managed() {
+			@Override
+			public void start() throws Exception {
+			}
+
+			@Override
+			public void stop() throws Exception {
+				jedisPool.destroy();
+			}
+		});
+
+
+		// load redis config, setup redis connection pool
 		RateLimitConfiguration conf = getRateLimitConfiguration(configuration);
 		jedisPoolConfig = conf.poolConfig;
 		jedisPool = new JedisPool(jedisPoolConfig, conf.getRedisHost(), conf.getRedisPort());
-
 		environment.healthChecks().register("redis", new RedisHealthCheck(jedisPool));
 
+		// load redis lua script into script cache
+		URL url = Resources.getResource("SlidingWindowRateLimit.lua");
+		String slidingWindowRateLimitLua = Resources.toString(url, Charsets.UTF_8);
+		Jedis jedis = jedisPool.getResource();
+		jedis.scriptLoad(slidingWindowRateLimitLua);
+		jedisPool.returnResource(jedis);
+
+		// register the filter
 		RateLimitFilter rateLimitFilter = new RateLimitFilter(jedisPool);
 		environment.jersey().register(rateLimitFilter);
 	}
--- bundle/src/main/java/ca/nlap/dropwizard/ratelimit/RateLimitFilter.java
+++ bundle/src/main/java/ca/nlap/dropwizard/ratelimit/RateLimitFilter.java
@@ -27,9 +27,47 @@ public class RateLimitFilter implements ContainerRequestFilter {
     public void filter(ContainerRequestContext requestContext) throws IOException
     {
         // check if client exceeded rate limit
-        if (!requestContext.getSecurityContext().isSecure())
+        if (isOverRateLimit())
         {
             requestContext.abortWith(Response.status(429).entity("Too Many Requests").build());
+        }
+    }
+
+    // testable method to do the rate limit check
+    private boolean isOverRateLimit() {
+        String requestIdentifier = generateRequestIdentifier();
+        if (requestIdentifier == null) {
+            return false;
+        }
+        Jedis jedis = jedisPool.getResource();
+
+        // build the parameters of redis job
+        ArrayList<String> keys = new ArrayList<String>();
+        keys.add(requestIdentifier);
+        ArrayList<String> params = new ArrayList<String>();
+        params.add("[[1, 10], [3, 200], [36, 3000]]");
+        Long unixTime = System.currentTimeMillis() / 1000L;
+        params.add(unixTime.toString());
+        params.add("1");
+
+        // run the script from cache, this is the sha1 hash of SlidingWindowRateLimit.lua
+        long result = (long)jedis.evalsha("08b03d80e639c5b33de9df638ca4fecbd727b1d0", keys, params);
+
+        jedisPool.returnResource(jedis);
+        if (result != 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    // generate a request identifier, either the IP address, or the user, if logged in
+    private String generateRequestIdentifier() {
+        if (request.getRemoteUser() != null) {
+            return "user:"+ request.getRemoteUser();
+        } else {
+            return "ip:"+ request.getRemoteAddr();
         }
     }
 
